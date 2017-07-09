26301f125559e6d65a2a328ca70f92c711a0c60e
plaban1981 <plaban1981@users.noreply.github.com>
2017-07-09 09:57:27 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+	}
+	/* create a static block. this block will be executed only once when the 
+	 * class is loaded
+	 * 
+	 */
+	
+}
